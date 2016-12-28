7fb1eef825cccf46801f6af38693f15e5404e69c
wz2793312458 <wz2793312458@163.com>
2016-12-28 18:16:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# -SwipeCaptcha
+Swipe captcha of Android platform。
+Android 平台的滑动验证码。
+
+**在 Android端app上，自定义View，仿一个斗鱼web端滑动验证码**。
+博文：http://blog.csdn.net/zxt0601/article/details/53315975
+
+![我们的Demo,Ac娘镇楼](http://ac-mhke0kuv.clouddn.com/7fcb58653e358b9ec003.gif)
+
+# Usage 用法：
+Step 1. Add the JitPack repository to your build file
+在项目根build.gradle文件中增加JitPack仓库依赖。
+```
+    allprojects {
+		repositories {
+			...
+			maven { url "https://jitpack.io" }
+		}
+	}
+```
+Step 2. Add the dependency
+```
+    dependencies {
+	        compile 'com.github.mcxtzhang:SwipeCaptcha:V1.0.0'
+	}
+```
+
+
+Step 3. 
+```
+    <com.mcxtzhang.captchalib.SwipeCaptchaView
+        android:id="@+id/swipeCaptchaView"
+        android:layout_width="300dp"
+        android:layout_height="200dp"
+        android:layout_centerHorizontal="true"
+        android:scaleType="centerCrop"
+        tools:src="@drawable/pic11"
+        app:captchaHeight="30dp"
+        app:captchaWidth="30dp"/>
+
+    <SeekBar
+        android:id="@+id/dragBar"
+        android:layout_width="320dp"
+        android:layout_height="60dp"
+        android:layout_below="@id/swipeCaptchaView"
+        android:layout_centerHorizontal="true"
+        android:layout_marginTop="30dp"
+        android:progressDrawable="@drawable/dragbg"
+        android:thumb="@drawable/thumb_bg"/>
+```
+
+
+
+
+那么本控件包含不仅包含以下功能：
+* 随机区域**起点(左上角x,y)**生成一个验证码阴影。
+* 验证码拼图 **凹凸图形**会随机变换。
+* 验证码区域**宽高**可自定义。
+* **抠图**验证码区域，绘制一个用于联动滑动的验证码滑块。
+* 验证失败，会闪烁几下然后回到原点。
+* 验证成功，会有白光扫过的动画。
+
+分解一下验证码核心实现思路：
+*  控件继承自ImageView。理由：
+1 如果放在项目中用，验证码图片希望可以是接口返回。ImageView以及其子类支持花式加载图片。
+2 继承自ImageView，绘制图片本身不用我们干预，也不用我们操心scaleType，节省很多工作。
+* 在`onSizeChanged()`方法中生成 和 控件宽高相关的属性值：
+1 初始化时随机生成验证码区域起点
+2 生成验证码区域Path
+3 生成滑块Bitmap
+* `onDraw()`时，依次绘制：
+1 验证码阴影
+2 滑块
+
+# to do list,待完善
+* abstract dragbar(seekbar) interface
+* SwipeCaptcha inside to hold the interface to do something
+
+* 抽象拖动条接口
+* SwipeCaptcha内部要持有这个接口 做一些事情
---
+++ app/src/main/java/com/mcxtzhang/swipecaptcha/MainActivity.java
@@ -0,0 +1,82 @@
+package com.mcxtzhang.swipecaptcha;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.mcxtzhang.captchalib.SwipeCaptchaView;
+
+public class MainActivity extends AppCompatActivity {
+    SwipeCaptchaView mSwipeCaptchaView;
+    SeekBar mSeekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSwipeCaptchaView = (SwipeCaptchaView) findViewById(R.id.swipeCaptchaView);
+        mSeekBar = (SeekBar) findViewById(R.id.dragBar);
+        findViewById(R.id.btnChange).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSwipeCaptchaView.createCaptcha();
+                mSeekBar.setEnabled(true);
+                mSeekBar.setProgress(0);
+            }
+        });
+        mSwipeCaptchaView.setOnCaptchaMatchCallback(new SwipeCaptchaView.OnCaptchaMatchCallback() {
+            @Override
+            public void matchSuccess(SwipeCaptchaView swipeCaptchaView) {
+                Toast.makeText(MainActivity.this, "恭喜你啊 验证成功 可以搞事情了", Toast.LENGTH_SHORT).show();
+                //swipeCaptcha.createCaptcha();
+                mSeekBar.setEnabled(false);
+            }
+
+            @Override
+            public void matchFailed(SwipeCaptchaView swipeCaptchaView) {
+                Log.d("zxt", "matchFailed() called with: swipeCaptchaView = [" + swipeCaptchaView + "]");
+                Toast.makeText(MainActivity.this, "你有80%的可能是机器人，现在走还来得及", Toast.LENGTH_SHORT).show();
+                swipeCaptchaView.resetCaptcha();
+                mSeekBar.setProgress(0);
+            }
+        });
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mSwipeCaptchaView.setCurrentSwipeValue(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //随便放这里是因为控件
+                mSeekBar.setMax(mSwipeCaptchaView.getMaxSwipeValue());
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("zxt", "onStopTrackingTouch() called with: seekBar = [" + seekBar + "]");
+                mSwipeCaptchaView.matchCaptcha();
+            }
+        });
+
+        //测试从网络加载图片是否ok
+        Glide.with(this)
+                .load("http://www.investide.cn/data/edata/image/20151201/20151201180507_281.jpg")
+                .asBitmap()
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                        mSwipeCaptchaView.setImageBitmap(resource);
+                        mSwipeCaptchaView.createCaptcha();
+                    }
+                });
+    }
+}
